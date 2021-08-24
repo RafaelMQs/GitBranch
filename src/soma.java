137d76169d5ae0293ace97994fb0824c6bad58de
RafaelMQs <rafaeelmq08@gmail.com>
2021-08-24 16:52:17 -0300

    Criando o repositorio do projeto NotePad

---
+++ src/soma.java
@@ -0,0 +1,12 @@
+
+public class soma {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 5;
+		int soma = num1 + num2;
+		
+		System.out.println("A soma dos dois numeros é: "+soma);
+	}
+
+}
