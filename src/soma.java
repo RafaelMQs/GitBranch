703d8b4e844de78bd5c68e894178d165f290eb8c
RafaelMQs <rafaeelmq08@gmail.com>
2021-08-24 17:01:52 -0300

    Alterando soma e add subtração

--- src/soma.java
+++ src/soma.java
@@ -4,9 +4,11 @@ public class soma {
 	public static void main(String[] args) {
 		int num1 = 10;
 		int num2 = 5;
-		int soma = num1 + num2;
 		
-		System.out.println("A soma dos dois numeros é: "+soma);
+		System.out.println("A soma dos dois numeros é: "+(num1 + num2));
+		System.out.println("A subtração dos dois numeros é: "+(num1 - num2));
+		
+		
 	}
 
 }
